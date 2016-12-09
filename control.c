f2162550a67467951b0fcbfef4cc40950f44df7c
jchua1 <jchua@stuy.edu>
2016-12-09 01:24:10 -0800

    basic functionality of -c, -r, -v

--- control.c
+++ control.c
@@ -6,28 +6,34 @@
 #include <sys/types.h>
 #include <sys/sem.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char * argv[]) {
 
   int sd;
-  int sem;
+  int semid;
   int sc;
   int key = ftok("makefile", 22);
 
   if (strncmp(argv[1], "-c", strlen(argv[1])) == 0) {
     sd = shmget(key, sizeof(int), IPC_CREAT | IPC_EXCL | 0644);
-    sem = semget(key, 1, IPC_CREAT | IPC_EXCL | 0644);
+    semid = semget(key, 1, IPC_CREAT | IPC_EXCL | 0644);
+    open("telephone", O_CREAT | O_TRUNC, 0644);
   }
-
   else if (strncmp(argv[1], "-r", strlen(argv[1])) == 0) {
-    sd = shmget(key, sizeof(int), IPC_CREAT | IPC_EXCL | 0644);
-    
-    sem = semget(key, 1, 0);
-    sc = semctl(sem, 0, IPC_RMID);
+    sd = shmget(key, sizeof(int), IPC_CREAT | 0644);
+    semid = semget(key, 1, 0);
+    shmctl(sd, IPC_RMID, 0);
+    semctl(semid, 0, IPC_RMID);
   }
-
   else if (strncmp(argv[1], "-v", strlen(argv[1])) == 0) {
-
+    int f = open("telephone", O_RDONLY);
+    int b[100];
+    read(f, b, sizeof(b));
+    int i;
+    for (i = 0; i < sizeof(b); i++)
+      printf("%c", b[i]);
+      close(f);
   }
   
   return 0;

7df98d9c0d75891848f605a4697a20dfb6acf2b3
jchua1 <jchua@stuy.edu>
2016-12-09 15:33:31 -0500

    finished commands?

--- control.c
+++ control.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <sys/shm.h>
 #include <sys/ipc.h>
 #include <sys/types.h>
 #include <sys/sem.h>
 #include <fcntl.h>
 #include <unistd.h>
+
+void display() {
+  struct stat stats;
+  stat("telephone", &stats);
+  int size = stats.st_size;
+  int f = open("telephone", O_RDONLY);
+  char b[size];
+  read(f, b, size);
+  int i;
+  for (i = 0; i < size; i++)
+    printf("%c", b[i]);
+  printf("\n");
+  close(f);
+}
 
 int main(int argc, char * argv[]) {
 
@@ -18,6 +33,7 @@ int main(int argc, char * argv[]) {
   if (strncmp(argv[1], "-c", strlen(argv[1])) == 0) {
     sd = shmget(key, sizeof(int), IPC_CREAT | IPC_EXCL | 0644);
     semid = semget(key, 1, IPC_CREAT | IPC_EXCL | 0644);
+    semctl(semid, 0, SETVAL, 1);
     open("telephone", O_CREAT | O_TRUNC, 0644);
   }
   else if (strncmp(argv[1], "-r", strlen(argv[1])) == 0) {
@@ -25,15 +41,10 @@ int main(int argc, char * argv[]) {
     semid = semget(key, 1, 0);
     shmctl(sd, IPC_RMID, 0);
     semctl(semid, 0, IPC_RMID);
+    display();
   }
   else if (strncmp(argv[1], "-v", strlen(argv[1])) == 0) {
-    int f = open("telephone", O_RDONLY);
-    int b[100];
-    read(f, b, sizeof(b));
-    int i;
-    for (i = 0; i < sizeof(b); i++)
-      printf("%c", b[i]);
-      close(f);
+    display();
   }
   
   return 0;
